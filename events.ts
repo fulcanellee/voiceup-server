281d294bc889d9add2a1f639194bf409b29f4e31
Mikhail Lukin <mishalykin@mail.ru>
2024-10-20 22:02:13 +0700

    implement mvp

---
+++ events.ts
@@ -0,0 +1,11 @@
+export enum EVENTS {
+  EMIT_ROOMS = "emit_rooms",
+  JOIN = "join",
+  ADD_PEER = "add_peer",
+  REMOVE_PEER = "remove_peer",
+  LEAVE_ROOM = "leave_room",
+  RELAY_ICE = "relay_ice",
+  RELAY_SDP = "relay_sdp",
+  SESSION_DESCRIPTION = "session_description",
+  ICE_CANDIDATE = "ice_candidate",
+}
--- server.ts
+++ server.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import { createServer } from "node:http";
 import { Server } from "socket.io";
+import { EVENTS } from "./events";
+import { version, validate } from "uuid";
 
 const app = express();
 const server = createServer(app);
@@ -12,12 +14,91 @@ const io = new Server(server, {
   },
 });
 
-io.on("connection", (ws) => {
-  console.log("User connected");
+const getRooms = () => {
+  const { rooms } = io.sockets.adapter;
 
-  ws.on("message", (data) => {
+  return Array.from(rooms.keys()).filter(
+    (id) => validate(id) && version(id) === 4
+  );
+};
+
+const updateRoomsData = () => {
+  io.emit(EVENTS.EMIT_ROOMS, {
+    rooms: getRooms(),
+  });
+};
+
+io.on("connection", (socket) => {
+  updateRoomsData();
+
+  socket.on("message", (data) => {
     console.log("Handle user message:", data);
   });
+
+  socket.on(EVENTS.JOIN, (config) => {
+    const { room: roomID } = config;
+    const { rooms: joinedRooms } = socket;
+
+    if (Array.from(joinedRooms).includes(roomID)) {
+      return console.warn(`Already joined to ${roomID}`);
+    }
+
+    const clients = Array.from(io.sockets.adapter.rooms.get(roomID) || []);
+
+    clients.forEach((clientID) => {
+      io.to(clientID).emit(EVENTS.ADD_PEER, {
+        peerID: socket.id,
+        createOffer: false,
+      });
+
+      socket.emit(EVENTS.ADD_PEER, {
+        peerID: clientID,
+        createOffer: true,
+      });
+    });
+
+    socket.join(roomID);
+    updateRoomsData();
+  });
+
+  const leaveRoom = () => {
+    const { rooms } = socket;
+
+    Array.from(rooms).forEach((roomID) => {
+      const clients = Array.from(io.sockets.adapter.rooms.get(roomID) || []);
+
+      clients.forEach((clientID) => {
+        io.to(clientID).emit(EVENTS.REMOVE_PEER, {
+          peerID: socket.id,
+        });
+
+        socket.emit(EVENTS.REMOVE_PEER, {
+          peerID: clientID,
+        });
+      });
+
+      socket.leave(roomID);
+    });
+
+    updateRoomsData();
+  };
+
+  socket.on(EVENTS.LEAVE_ROOM, leaveRoom);
+  socket.on("disconnecting", leaveRoom);
+
+  socket.on(EVENTS.RELAY_SDP, ({peerID, sessionDescription}) => {
+    io.to(peerID).emit(EVENTS.SESSION_DESCRIPTION, {
+      peerID: socket.id,
+      sessionDescription
+    })
+  });
+
+  socket.on(EVENTS.RELAY_ICE, ({peerID, iceCandidate}) => {
+    io.to(peerID).emit(EVENTS.ICE_CANDIDATE, {
+      peerID: socket.id,
+      iceCandidate,
+    })
+  })
 });
 
 server.listen(PORT, () => {
